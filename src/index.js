6403548c688f13a4fc6fd33c5a2c1a4efaa59d4c
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-04-26 17:04:48 +0000

    make main

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import './style.css'
 import loadPage from './mainPage';
 
 function main() {
-
-    const mainSection = loadPage()
+    
+    const mainSection = loadPage();
 
     return mainSection;
 }
-
-const elementId = document.querySelector('#content');
 
 document.body.appendChild(main())
--- src/mainPage.js
+++ src/mainPage.js
@@ -1,3 +1,11 @@
 export default function loadPage() {
+
+    const element = document.querySelector("#content");
+    const createText = document.createElement('div');
+    createText.innerHTML = "Test";
+
+    element.appendChild(createText);
     
+    return element
+
 }
