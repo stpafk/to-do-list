6e0fa60710cab18f3139235dc6f86465263a90e0
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-04-27 11:12:55 +0000

    start js logic

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import loadPage from './mainPage';
 function main() {
     
     const mainSection = loadPage();
-
+    // something like if there is not more than 5 to do stuff, 
     return mainSection;
 }
 
